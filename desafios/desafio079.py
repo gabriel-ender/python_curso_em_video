fcc83f1ab4ab84fd2f5972110683c0294f4f4f0f
gabriel-ender <gabrielender.md@gmail.com>
2026-01-21 19:12:14 -0400

    Alteração do desafio079

--- desafios/desafio079.py
+++ desafios/desafio079.py
@@ -4,9 +4,15 @@
 # os valores únicos digitados, em ordem crescente.
 lista1 = list()
 pergunta = 'S'
+a = ""
 while pergunta in 'S':
-    lista1.append(int(input('Digite um valor: ')))
+    a = int(input(f"Digite um valor: "))
+    if a not in lista1:
+        lista1.append(a)
+    else:
+        print(f"O número {a} já existe na lista. Valor NÃO computado.")
     pergunta = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
     while pergunta not in 'NS':
         pergunta = str(input('Opção Inválida! Deseja continuar? [S/N]: ')).strip().upper()[0]
 print(f'Lista 1: {sorted(lista1)}')
+print("FIM DO PROGRAMA!")
